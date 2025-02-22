9aaa00bf06bbf65515a026a31fa67633f292b90c
Hana <fweriuhb@gmail.com>
2025-02-22 23:13:21 +0900

    注册表单页面

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PagesController;
 
@@ -15,3 +16,5 @@ use App\Http\Controllers\PagesController;
 */
 
 Route::get('/', [PagesController::class, 'root'])->name('root');
+
+Auth::routes();
