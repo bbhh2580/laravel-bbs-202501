babb7f16e70ed339c0a9aca863c377db616e7820
Hana <fweriuhb@gmail.com>
2025-02-28 15:40:59 +0900

    填充用户和话题数据

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
@@ -15,14 +16,24 @@ class UserFactory extends Factory
      *
      * @return array<string, mixed>
      */
-    public function definition()
+    public function definition(): array
     {
+       $avatars = [
+           config('app.url') . '/uploads/images/default-avatars/200.jpg',
+           config('app.url') . '/uploads/images/default-avatars/300.jpg',
+           config('app.url') . '/uploads/images/default-avatars/400.jpg',
+           config('app.url') . '/uploads/images/default-avatars/500.jpg',
+           config('app.url') . '/uploads/images/default-avatars/600.jpg',
+       ];
+
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'introduction' => $this->faker->sentence(),
+            'avatar' => $this->faker->randomElement($avatars),
         ];
     }
 
---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        // 生成10个用户
+        User::factory()->count(10)->create();
+
+        // 单独处理第一个用户数据，以供测试
+        $user = User::find(1);
+        $user->name = 'hana';
+        $user->email = 'na3540563@gmail.com';
+        $user->password = bcrypt('11111111');
+        $user->avatar =config('app.url') . 'https://picsum.photos/200/300';
+        $user->save();
+    }
+}
