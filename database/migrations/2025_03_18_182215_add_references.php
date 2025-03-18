34c1cafce3246415ac435271d189b07bb3fbe97a
Hana <fweriuhb@gmail.com>
2025-03-18 19:15:59 +0900

    联动删除

--- database/migrations/2025_03_18_182215_add_references.php
+++ database/migrations/2025_03_18_182215_add_references.php
@@ -11,9 +11,24 @@ return new class extends Migration
      *
      * @return void
      */
-    public function up()
+    public function up(): void
     {
-        //
+        // 创建外键约束
+
+        Schema::table('topics', function (Blueprint $table) {
+
+            // 当user_id 对应的 users表中的用户被删除的时候，就会删除对应该用户的所有话题
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+
+        Schema::table('replies', function (Blueprint $table) {
+
+            // 当user_id 对应的 users 表中的用户被删除的时候，就会删除对应该用户的所有回复
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            // 当topic_id 对应的 topics 表中的话题被删除的时候，就会删除对应该话题的所有回复
+            $table->foreign('topic_id')->references('id')->on('topics')->onDelete('cascade');
+        });
     }
 
     /**
@@ -21,8 +36,17 @@ return new class extends Migration
      *
      * @return void
      */
-    public function down()
+    public function down(): void
     {
-        //
+        // 移除外键约束
+
+        Schema::table('topics', function (Blueprint $table) {
+            $table->dropForeign('user_id');
+        });
+
+        Schema::table('replies', function (Blueprint $table) {
+            $table->dropForeign('user_id');
+            $table->dropForeign('topic_id');
+        });
     }
 };
--- resources/views/topics/_sidebar.blade.php
+++ resources/views/topics/_sidebar.blade.php
@@ -25,39 +25,19 @@
         </div>
     </div>
 @endif
-{{--@if(count($active_users))--}}
-{{--    <div class="card mt-4">--}}
-{{--        <div class="card-body active-users pt-2">--}}
-{{--            <div class="text-center mt-1 mb-0 text-muted">Active Users</div>--}}
-{{--            <hr class="mt-2">--}}
-{{--            @foreach($active_users as $user)--}}
-{{--                <a class="d-flex mt-2 text-decoration-none ms-3" href="{{ route('users.show', $user->id) }}">--}}
-{{--                    <div class="media-left media-middle me-2 ms-1">--}}
-{{--                        <img src="{{ $user->avatar }}" class="media-object" width="24px" height="24px"--}}
-{{--                             alt="{{ $user->name }}">--}}
-{{--                    </div>--}}
-{{--                    <div class="media-body ms-1">--}}
-{{--                        <small class="media-heading text-secondary">{{ $user->name }}</small>--}}
-{{--                    </div>--}}
-{{--                </a>--}}
-{{--            @endforeach--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--@endif--}}
 
-
-{{--@if(count($links))--}}
-{{--    <div class="card mt-4">--}}
-{{--        <div class="card-body pt-2">--}}
-{{--            <div class="text-center mt-1 mb-0 text-muted">Links</div>--}}
-{{--            <hr class="mt-2">--}}
-{{--            @foreach($links as $link)--}}
-{{--                <a class="d-flex mt-2 text-decoration-none ms-3" href="{{ $link->link }}">--}}
-{{--                    <div class="media-body">--}}
-{{--                        <span class="media-heading text-muted">{{ $link->title }}</span>--}}
-{{--                    </div>--}}
-{{--                </a>--}}
-{{--            @endforeach--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--@endif--}}
+@if(count($links))
+    <div class="card mt-4">
+        <div class="card-body pt-2">
+            <div class="text-center mt-1 mb-0 text-muted">Links</div>
+            <hr class="mt-2">
+            @foreach($links as $link)
+                <a class="d-flex mt-2 text-decoration-none ms-3" href="{{ $link->link }}">
+                    <div class="media-body">
+                        <span class="media-heading text-muted">{{ $link->title }}</span>
+                    </div>
+                </a>
+            @endforeach
+        </div>
+    </div>
+@endif
