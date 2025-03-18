4709ec03eeadbbf386638a2bdb34da9fd87982a4
Hana <fweriuhb@gmail.com>
2025-03-18 15:28:48 +0900

    活跃用户

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Link;
 use App\Models\Topic;
+use App\Models\User;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
@@ -17,6 +19,8 @@ class CategoriesController extends Controller
      * @param Category $category
      * @param Request $request
      * @param Topic $topic
+     * @param User $user
+     * @param Link $link
      * @return Factory|View|Application
      */
     public function show(Category $category, Request $request, Topic $topic): Factory|View|Application
@@ -25,6 +29,9 @@ class CategoriesController extends Controller
             ->where('category_id', $category->id)
             ->with('user', 'category') // 使用with方法预加载防止 N+1 问题
             ->paginate(20);
+
+        $active_users = $user->getActiveUsers();
+        $links = $link->getAllCached();
         return view('topics.index', compact('topics', 'category'));
     }
 }
